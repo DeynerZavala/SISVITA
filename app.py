8bfb141cfc2332c3518ceff879f978e713d38614
DeynerZavala <dyr.zva.hyi@gmail.com>
2024-07-02 19:51:47 -0500

    - cus 3.1

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 
+from routes.ansiedad_routes import ansiedad_routes
 from routes.diagnostico_routes import diagnostico_routes
 from routes.ansiedad_semaforo_routes import ansiedad_semaforo_routes
 from routes.opciones_routes import opciones_routes
@@ -36,6 +37,7 @@ app.register_blueprint(titulo_routes)
 app.register_blueprint(ansiedad_semaforo_routes)
 app.register_blueprint(ubigeo_routes)
 app.register_blueprint(diagnostico_routes)
+app.register_blueprint(ansiedad_routes)
 
 @app.route('/')
 def hello_world():  # put application's code here
--- schemas/ansiedad_schema.py
+++ schemas/ansiedad_schema.py
@@ -4,7 +4,7 @@ from models.ansiedad import Ansiedad
 class AnsiedadSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Ansiedad
-        fields = ('ansiedad_id','nivel')
+        fields = ('ansiedad_id','nivel','ans_sem_id')
         
 ansiedad_schema = AnsiedadSchema()
 ansiedades_schema = AnsiedadSchema(many=True)
