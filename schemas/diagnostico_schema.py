86245dd258bb362cb23d14abe3170e22e3353d8f
DeynerZavala <dyr.zva.hyi@gmail.com>
2024-07-03 00:48:46 -0500

    - cus 3.1

--- schemas/diagnostico_schema.py
+++ schemas/diagnostico_schema.py
@@ -4,7 +4,7 @@ from models.diagnostico import Diagnostico
 class DiagnosticosSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Diagnostico
-        fields = ('diagnostico_id','especialista_id', 'ansiedad_id','fecha',
+        fields = ('diagnostico_id','diagnostico_id', 'ansiedad_id','fecha',
                   'comunicacion_estudiante', 'solicitar_cita','tratamiento_id','fundamentacion_cientifica')
 diagnostico_schema = DiagnosticosSchema()
 diagnosticos_schema = DiagnosticosSchema(many=True)
